1c2faa3334ba6fc5b647a90562c7ff25754d5da6
개발자1 <cm41823@gmail.com>
2026-04-20 17:09:11 +0900

    덧셈 블루 케이스 (리팩토링)통과

--- src/main/java/com/ll/Calc.java
+++ src/main/java/com/ll/Calc.java
@@ -3,17 +3,18 @@ package com.ll;
 public class Calc {
     public static int run(String expression) {
 
-        String[] expressionBits = expression.split(" \\+ ");
+        if (expression.contains("+")) {
+            String[] bits = expression.split(" \\+ ");
+            return Integer.parseInt(bits[0]) + Integer.parseInt(bits[1]);
+        }
+
         // 뺄셈 처리
         if (expression.contains("-")) {
             String[] bits = expression.split(" - ");
             return Integer.parseInt(bits[0]) - Integer.parseInt(bits[1]);
         }
 
-            int num1 = Integer.parseInt(expressionBits[0]);
-        int num2 = Integer.parseInt(expressionBits[1]);
-
-        return num1 + num2;
+    return 0;
     }
 
 }
