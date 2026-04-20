8a817b085f7e85c79c00b73b3cff6b8cdd8937a5
개발자1 <cm41823@gmail.com>
2026-04-20 16:43:51 +0900

    ã테스트 코드 t5~t28 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,6 +12,7 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+   //assertThat을 사용하기 위한 의존성 추가
     testImplementation("org.assertj:assertj-core:3.27.3")
 }
 
