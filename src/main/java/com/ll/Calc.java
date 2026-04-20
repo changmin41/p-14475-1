da9fd876647d51dafa2547fcf1c99b2b2d1849b2
개발자1 <cm41823@gmail.com>
2026-04-21 07:48:48 +0900

    [t14~t17] blue 통과

--- src/main/java/com/ll/Calc.java
+++ src/main/java/com/ll/Calc.java
@@ -5,13 +5,30 @@ public class Calc {
         // 공백 기준으로 분할
         String[] Blank = expression.split(" ");
 
+        //곱셈 먼저 계산한다. 돌면서 곱셈을 먼저 찾는다
+        String[] afterMulti = new String[Blank.length];
+        int count = 0;
+
+        for (int i = 0; i < Blank.length; i++) {
+            if (i + 2 < Blank.length && Blank[i + 1].equals("*")) {
+
+                int product = Integer.parseInt(Blank[i]) * Integer.parseInt(Blank[i + 2]);
+                afterMulti[count++] = String.valueOf(product);
+                i += 2;
+            } else {
+                afterMulti[count++] = Blank[i];
+            }
+        }
+        String[] Blank2 = new String[count];
+        System.arraycopy(afterMulti, 0, Blank2, 0, count);
+
         // 첫 번째 숫자로 시작
-        int result = Integer.parseInt(Blank[0]);
+        int result = Integer.parseInt(Blank2[0]);
 
         // 연산자랑 합쳐서 계산하기
-        for (int i = 1; i < Blank.length; i += 2) {
-            String operator = Blank[i];
-            int num = Integer.parseInt(Blank[i + 1]);
+        for (int i = 1; i < Blank2.length; i += 2) {
+            String operator = Blank2[i];
+            int num = Integer.parseInt(Blank2[i + 1]);
 
             if (operator.equals("+")) {
                 result += num;
