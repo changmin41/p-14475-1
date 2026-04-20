5b9b77938bdc5f0a6d95f772c2b500f186941866
개발자1 <cm41823@gmail.com>
2026-04-20 17:30:00 +0900

    8t 리팩토링 통과

--- src/main/java/com/ll/Calc.java
+++ src/main/java/com/ll/Calc.java
@@ -3,16 +3,13 @@ package com.ll;
 public class Calc {
     public static int run(String expression) {
 
-
-
-        if(expression.equals("10 + 20 + 30")) return 60;
-
-
-
         // 덧셈 처리
         if (expression.contains("+")) {
+            int sum = 0;
             String[] bits = expression.split(" \\+ ");
-            return Integer.parseInt(bits[0]) + Integer.parseInt(bits[1]);
+            for(int i=0; i< bits.length;i++){
+              sum += Integer.parseInt(bits[i]);
+           }return sum;
         }
 
         // 뺄셈 처리
